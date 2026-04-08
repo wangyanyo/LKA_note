e279c1eb17891019ec219c76b8690e1a62e0703e
WangYanYo <2378130965@qq.com>
2026-04-08 22:47:52 +0800

    26.4.8

--- Kernel/src/kernel.c
+++ Kernel/src/kernel.c
@@ -4,6 +4,7 @@
 #include "io/io.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "disk/disk.h"
 
 struct paging_4gb_chunk *kernel_chunk = 0;
 
@@ -31,6 +32,11 @@ void kernel_main()
 	ptr2[3] = 0;
 	
 	terminal_print(ptr2);
+	terminal_print(ptr);
+
+	char buf[512];
+    	disk_read_sector(0, 1, buf);
+
 	terminal_print(ptr);
 
 	enable_interrupts();
