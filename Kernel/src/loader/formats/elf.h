fc3c4fb0400feeaeeb134d6bf71d6176cc6b3a13
WangYanYo <2378130965@qq.com>
2026-09-06 19:40:32 +0800

    Implementing Elf Loader - Part 2

--- Kernel/src/loader/formats/elf.h
+++ Kernel/src/loader/formats/elf.h
@@ -1,5 +1,5 @@
-#ifndef ELF_H
-#define ELF_H
+#ifndef __LOADER_FORMATS_ELF_H
+#define __LOADER_FORMATS_ELF_H
 
 #include <stdint.h>
 #include <stddef.h>
@@ -61,67 +61,70 @@ typedef int32_t elf32_off;
 
 struct elf32_phdr
 {
-    elf32_word p_type;
-    elf32_off p_offset;
-    elf32_addr p_vaddr;
-    elf32_addr p_paddr;
-    elf32_word p_filesz;
-    elf32_word p_memsz;
-    elf32_word p_flags;
-    elf32_word p_align;
+	elf32_word p_type;
+	elf32_off p_offset;
+	elf32_addr p_vaddr;
+	elf32_addr p_paddr;
+	elf32_word p_filesz;
+	elf32_word p_memsz;
+	elf32_word p_flags;
+	elf32_word p_align;
 } __attribute__((packed));
 
 struct elf32_shdr
 {
-    elf32_word sh_name;
-    elf32_word sh_type;
-    elf32_word sh_flags;
-    elf32_addr sh_addr;
-    elf32_off sh_offset;
-    elf32_word sh_size;
-    elf32_word sh_link;
-    elf32_word sh_info;
-    elf32_word sh_addralign;
-    elf32_word sh_entsize;
+	elf32_word sh_name;
+	elf32_word sh_type;
+	elf32_word sh_flags;
+	elf32_addr sh_addr;
+	elf32_off sh_offset;
+	elf32_word sh_size;
+	elf32_word sh_link;
+	elf32_word sh_info;
+	elf32_word sh_addralign;
+	elf32_word sh_entsize;
 } __attribute__((packed));
 
 struct elf_header
 {
-    unsigned char e_ident[EI_NIDENT];
-    elf32_half e_type;
-    elf32_half e_machine;
-    elf32_word e_version;
-    elf32_addr e_entry;
-    elf32_off e_phoff;
-    elf32_off e_shoff;
-    elf32_word e_flags;
-    elf32_half e_ehsize;
-    elf32_half e_phentsize;
-    elf32_half e_phnum;
-    elf32_half e_shentsize;
-    elf32_half e_shnum;
-    elf32_half e_shstrndx;
+	unsigned char e_ident[EI_NIDENT];
+	elf32_half e_type;
+	elf32_half e_machine;
+	elf32_word e_version;
+	elf32_addr e_entry;
+	elf32_off e_phoff;
+	elf32_off e_shoff;
+	elf32_word e_flags;
+	elf32_half e_ehsize;
+	elf32_half e_phentsize;
+	elf32_half e_phnum;
+	elf32_half e_shentsize;
+	elf32_half e_shnum;
+	elf32_half e_shstrndx;
 } __attribute__((packed));
 
 struct elf32_dyn
 {
-    elf32_sword d_tag;
-    union 
-    {
-        elf32_word d_val;
-        elf32_addr d_ptr;
-    } d_un;
+	elf32_sword d_tag;
+	union 
+	{
+		elf32_word d_val;
+		elf32_addr d_ptr;
+	} d_un;
     
 } __attribute__((packed));
 
 struct elf32_sym
 {
-    elf32_word st_name;
-    elf32_addr st_value;
-    elf32_word st_size;
-    unsigned char st_info;
-    unsigned char st_other;
-    elf32_half st_shndx;
+	elf32_word st_name;
+	elf32_addr st_value;
+	elf32_word st_size;
+	unsigned char st_info;
+	unsigned char st_other;
+	elf32_half st_shndx;
 } __attribute__((packed));
+
+void* elf_get_entry_ptr(struct elf_header* elf_header);
+uint32_t elf_get_entry(struct elf_header* elf_header);
 
 #endif
---
+++ Kernel/src/loader/formats/elfloader.h
@@ -0,0 +1,42 @@
+#ifndef __LOADER_FORMATS_ELFLOADER_H
+#define __LOADER_FORMATS_ELFLOADER_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "elf.h"
+#include "config.h"
+
+struct elf_file
+{
+	char filename[KERNEL_MAX_PATH];
+
+	int in_memory_size;
+
+	/**
+	 * The physical memory address that this elf file is loaded at
+	 */
+	void* elf_memory;
+
+	/**
+	 * The virtual base address of this binary
+	 */
+	void* virtual_base_address;
+
+	/**
+	 * The ending virtual address
+	 */
+	void* virtual_end_address;
+
+	/**
+	 * The physical base address of this binary
+	 */
+	void* physical_base_address;
+
+	/**
+	 * The physical end address of this bunary
+	 */
+	void* physical_end_address;
+};
+
+#endif
