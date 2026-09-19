93365500dddd41a9789fe10294ef7c015a63da17
WangYanYo <2378130965@qq.com>
2026-09-19 15:21:02 +0800

    Creating some  important stdlib functions

--- Kernel/programs/blank/blank.c
+++ Kernel/programs/blank/blank.c
@@ -1,9 +1,20 @@
 #include "kernel.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
+#include "memory.h"
 
 int main(int argc, char **argv)
 {
 	printf("My age is %i\n", 23);
+	char words[] = "hello how are you";
+
+	const char* token = strtok(words, " ");
+	while(token) {
+		printf("%s\n", token);
+		token = strtok(NULL, " ");
+	}
+
+	while(1) {}
 	return 0;
 }
