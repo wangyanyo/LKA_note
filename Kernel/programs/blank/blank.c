f6963c1f4b97e6830e19162c7fb510036a236de0
WangYanYo <2378130965@qq.com>
2026-09-19 16:54:49 +0800

    Parsing command arguments - Part 1

--- Kernel/programs/blank/blank.c
+++ Kernel/programs/blank/blank.c
@@ -6,13 +6,10 @@
 
 int main(int argc, char **argv)
 {
-	char *ptr = malloc(20);
-	strcpy(ptr, "Hello World!\n");
-	print(ptr);
-
-	free(ptr);
-
-	print("abc\n");
+	char str[] = "hello world";
+	struct command_argument* root_command = peachos_parse_command(str, sizeof(str));
+	printf("%s\n", root_command->argument);
+	printf("%s\n", root_command->next->argument);
 
 	while(1) {}
 	return 0;
--- Kernel/programs/stdlib/src/kernel.h
+++ Kernel/programs/stdlib/src/kernel.h
@@ -3,6 +3,11 @@
 
 #include <stddef.h>
 
+struct command_argument {
+	char argument[512];
+	struct command_argument* next;
+};
+
 void print(char *msg);
 int kernel_getkey();
 void kernel_putchar(char c);
@@ -12,5 +17,6 @@ void kernel_free(void *ptr);
 
 void kernel_terminal_readline(char *out, int max, int output_while_typing);
 int kernel_process_load_start(const char *filename);
+struct command_argument* peachos_parse_command(const char* command, int max);
 
 #endif
